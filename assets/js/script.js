272502fcfca979c6b9b063fb1ae3059553c5f1ac
kedi1991 <okurut.henry@yahoo.com>
2022-09-26 23:04:45 +0000

    add dsplay for speed, altitude and thrust

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,22 +7,41 @@ document.addEventListener("DOMContentLoaded", function(){
     var num1 = 0;
     var num2 = 0;
 
+    var speed = 0;
+    var altitude = 0;
+
 document.addEventListener("keydown", function(e){
     
     if(e.key === "ArrowUp"){
         console.log("Ascending ...");
         plane.style.marginTop = num1-- + "px";
+        (document.getElementById("altimeter")).value = altitude++;
     }else if (e.key == "ArrowLeft"){
         console.log("Decelerating ...");
+        (document.getElementById("speedometer")).value = speed--;
         plane.style.marginLeft = num1-- + "px";
     }else if (e.key == "ArrowRight"){
         console.log("Accelerating ...");
         plane.style.marginLeft = num1++ + "px";
+        (document.getElementById("speedometer")).value = speed++;
+
+        
+        (document.getElementById("accelerator")).value = accelerate();;
+        //accelerator key detection
+        function accelerate(){
+            setInterval( ++altitude, 10);
+            return altitude;
+        }
+        
+    
+
     }else if (e.key == "ArrowDown"){
         console.log("Descending ...");
         plane.style.marginTop = num1++ + "px";
+        (document.getElementById("altimeter")).value = altitude--;
     }else{
         console.log("Error: Unknown control ...");
+        
     }
     /**console.log(num1++);
 */
